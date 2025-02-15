0d2f1a1a06a6c2941fc1eb00030b1810dde0ca2a
MariaDolmatova <78088665+MariaDolmatova@users.noreply.github.com>
2025-02-16 01:39:04 +0300

    Update logger.py
    Signed-off-by: MariaDolmatova <78088665+MariaDolmatova@users.noreply.github.com>

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -1,24 +1,22 @@
 import logging
 import os
-from datetime import datetime
 
-# Define log directory and file
 LOG_DIR = "logs"
 os.makedirs(LOG_DIR, exist_ok=True)
-LOG_FILE = os.path.join(LOG_DIR, "main.log")
+LOG_FILE = os.path.join(LOG_DIR, "main.log") # define log directory and file
 
-# Configure the root logger
-logging.basicConfig(
-    level=logging.DEBUG,
-    format="%(asctime)s - %(filename)s - %(funcName)s - %(levelname)s - %(message)s",
-    handlers=[
-        logging.FileHandler(LOG_FILE, encoding='utf-8'),
-        logging.StreamHandler()
-    ]
-)
+if not logging.getLogger().handlers:
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s - %(filename)s - %(funcName)s - %(levelname)s - %(message)s",
+        handlers=[
+            logging.FileHandler(LOG_FILE, encoding='utf-8'),
+            logging.StreamHandler()
+        ]
+    ) # configure the root logger
 
-# Get the root logger
-logger = logging.getLogger()
+logger = logging.getLogger() # get the root logger
+logger.propagate = False
 
 def get_logger():
     return logger
